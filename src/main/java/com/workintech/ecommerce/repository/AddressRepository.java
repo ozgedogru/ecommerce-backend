5505efffd477a479bc5fa3c61d6cc98e08bb1d3a
ozgedogru <ozgenurkarademir@gmail.com>
2024-06-12 15:02:27 +0200

    adds a new method

--- src/main/java/com/workintech/ecommerce/repository/AddressRepository.java
+++ src/main/java/com/workintech/ecommerce/repository/AddressRepository.java
@@ -4,7 +4,9 @@ import com.workintech.ecommerce.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AddressRepository extends JpaRepository<Address, Long> {
     List<Address> findByUserId(Long userId);
+    Optional<Address> findByIdAndUserId(Long addressId, Long userId);
 }
